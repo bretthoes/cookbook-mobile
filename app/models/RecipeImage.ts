c675a010e6634e202e517320805c7677c1b39d5f
Brett Hoes <bretthoes@gmail.com>
2024-09-03 22:42:56 -0400

    add get image view

--- app/models/RecipeImage.ts
+++ app/models/RecipeImage.ts
@@ -1,5 +1,6 @@
 import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree";
 import { withSetPropAction } from "./helpers/withSetPropAction";
+import Config from "app/config";
 
 /**
  * This represents an image in a recipe.
@@ -12,6 +13,11 @@ export const RecipeImageModel = types
     ordinal: types.integer,
   })
   .actions(withSetPropAction)
+  .views((image) => ({
+    get getImage() {
+      return image.name ? `${Config.S3_URL}/${image.name}` : ""
+    },
+  }))
 
 export interface RecipeImage extends Instance<typeof RecipeImageModel> {}
 export interface RecipeImageSnapshotOut extends SnapshotOut<typeof RecipeImageModel> {}
