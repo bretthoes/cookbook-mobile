1186c8a2787ba191410cdf14be838d6a68e64d48
brett <bretthoes@gmail.com>
2024-09-15 00:34:00 -0400

    update add recipe screen

--- app/screens/AddRecipe.tsx
+++ app/screens/AddRecipe.tsx
@@ -1,13 +1,54 @@
-import React from "react"
-import { View, Text, TextInput, Button } from "react-native"
+import { Icon, TextField } from "app/components"
+import React, { useState } from "react"
+import { View, Text, Button } from "react-native"
 
 // TODO implement
+// TODO get text input component from template
 export const AddRecipeScreen = () => {
+  const [titleInput, setTitleInput] = useState("")
+  const [input, setInput] = useState("")
   return (
     <View>
       <Text>Add a New Recipe</Text>
-      <TextInput placeholder="Recipe Title" />
-      <TextInput placeholder="Ingredients" multiline />
+      <TextField 
+        value={titleInput}
+        onChangeText={(value) => setTitleInput(value)}
+        status="error"
+        labelTx="recipeAddScreen.titleLabel"
+        labelTxOptions={{ title: "Chicken Casserole" }}
+        LabelTextProps={{ style: { color: "#FFFFFF" } }}
+        placeholderTx="recipeAddScreen.titlePlacehoder"
+        placeholderTxOptions={{ title: "Chicken Casserole" }}
+        helper="Enter your name"
+        helperTx="recipeAddScreen.titleHelper"
+        helperTxOptions={{ title: "Chicken Casserole" }}
+        HelperTextProps={{ style: { color: "#FFFFFF" } }}
+        style={{ backgroundColor: "#BFBFBF" }}
+        containerStyle={{ backgroundColor: "#BFBFBF" }}
+        inputWrapperStyle={{ backgroundColor: "#BFBFBF" }}
+        RightAccessory={() => <Icon icon="check" />}
+        LeftAccessory={() => <Icon icon="bell" />}
+        />
+        <TextField 
+        value={input}
+        onChangeText={(value) => setInput(value)}
+        status="error"
+        labelTx="recipeAddScreen.titleLabel"
+        labelTxOptions={{ title: "Chicken Casserole" }}
+        LabelTextProps={{ style: { color: "#FFFFFF" } }}
+        placeholderTx="recipeAddScreen.titlePlacehoder"
+        placeholderTxOptions={{ title: "Chicken Casserole" }}
+        helper="Enter your name"
+        helperTx="recipeAddScreen.titleHelper"
+        helperTxOptions={{ title: "Chicken Casserole" }}
+        HelperTextProps={{ style: { color: "#FFFFFF" } }}
+        style={{ backgroundColor: "#BFBFBF" }}
+        containerStyle={{ backgroundColor: "#BFBFBF" }}
+        inputWrapperStyle={{ backgroundColor: "#BFBFBF" }}
+        RightAccessory={() => <Icon icon="check" />}
+        LeftAccessory={() => <Icon icon="bell" />}
+        multiline
+        />
       <Button title="Save Recipe" onPress={handleSaveRecipe} />
     </View>
   )
