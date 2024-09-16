20449bf10736ee748c9e1e7e5e0db5b02d141f54
Brett Hoes <bretthoes@gmail.com>
2024-09-15 23:42:48 -0400

    more input for add recipe

--- app/screens/AddRecipe.tsx
+++ app/screens/AddRecipe.tsx
@@ -1,58 +1,107 @@
-import { Icon, TextField } from "app/components"
+import { Button, TextField, Text, Screen } from "app/components"
+import { colors, spacing, typography } from "app/theme"
 import React, { useState } from "react"
-import { View, Text, Button } from "react-native"
+
+import { TextStyle, View, ViewStyle } from "react-native"
+import { DemoUseCase } from "./DemoShowroomScreen/DemoUseCase"
+import { DemoDivider } from "./DemoShowroomScreen/DemoDivider"
 
 // TODO implement
 // TODO get text input component from template
 export const AddRecipeScreen = () => {
   const [titleInput, setTitleInput] = useState("")
-  const [input, setInput] = useState("")
+  const [summaryInput, setSummaryInput] = useState("")
+  const [prepTimeInput, setPrepTimeInput] = useState("")
+  const [cookTimeInput, setCookTimeInput] = useState("")
+  const [servingsInput, setServingsInput] = useState("")
   return (
-    <View>
-      <Text>Add a New Recipe</Text>
-      <TextField 
+    <Screen
+      preset="fixed"
+      safeAreaEdges={["top"]}
+      contentContainerStyle={$screenContentContainer}
+    >
+      <View style={$titleContainer}>
+        <Text 
+          preset="heading"
+          weight="normal"
+          text="Add new recipe"
+        />
+        <Button
+          text="Save"
+          style={$saveButton}
+          onPress={handleSaveRecipe}
+        />
+      </View>
+
+      <DemoUseCase
+        name=""
+        description="Fill out the details for your new recipe."
+      >
+      <TextField
         value={titleInput}
         onChangeText={(value) => setTitleInput(value)}
-        status="error"
-        labelTx="recipeAddScreen.titleLabel"
-        labelTxOptions={{ title: "Chicken Casserole" }}
-        LabelTextProps={{ style: { color: "#FFFFFF" } }}
         placeholderTx="recipeAddScreen.titlePlacehoder"
-        placeholderTxOptions={{ title: "Chicken Casserole" }}
-        helper="Enter your name"
-        helperTx="recipeAddScreen.titleHelper"
-        helperTxOptions={{ title: "Chicken Casserole" }}
-        HelperTextProps={{ style: { color: "#FFFFFF" } }}
-        style={{ backgroundColor: "#BFBFBF" }}
-        containerStyle={{ backgroundColor: "#BFBFBF" }}
-        inputWrapperStyle={{ backgroundColor: "#BFBFBF" }}
-        RightAccessory={() => <Icon icon="check" />}
-        LeftAccessory={() => <Icon icon="bell" />}
-        />
-        <TextField 
-        value={input}
-        onChangeText={(value) => setInput(value)}
-        status="error"
-        labelTx="recipeAddScreen.titleLabel"
-        labelTxOptions={{ title: "Chicken Casserole" }}
-        LabelTextProps={{ style: { color: "#FFFFFF" } }}
-        placeholderTx="recipeAddScreen.titlePlacehoder"
-        placeholderTxOptions={{ title: "Chicken Casserole" }}
-        helper="Enter your name"
-        helperTx="recipeAddScreen.titleHelper"
-        helperTxOptions={{ title: "Chicken Casserole" }}
-        HelperTextProps={{ style: { color: "#FFFFFF" } }}
-        style={{ backgroundColor: "#BFBFBF" }}
-        containerStyle={{ backgroundColor: "#BFBFBF" }}
-        inputWrapperStyle={{ backgroundColor: "#BFBFBF" }}
-        RightAccessory={() => <Icon icon="check" />}
-        LeftAccessory={() => <Icon icon="bell" />}
+      />
+
+      <DemoDivider size={spacing.lg} />
+
+      <TextField
+        value={summaryInput}
+        onChangeText={(value) => setSummaryInput(value)}
+        placeholder="Summary (optional)"
         multiline
-        />
-      <Button title="Save Recipe" onPress={handleSaveRecipe} />
-    </View>
+      />
+
+      <DemoDivider size={spacing.lg} />
+
+      <TextField
+        value={prepTimeInput}
+        onChangeText={(value) => setPrepTimeInput(value)}
+        placeholder="Prep time in minutes"
+        inputMode="numeric"
+        keyboardType="numeric"
+      />
+
+      <DemoDivider size={spacing.lg} />
+
+      <TextField
+        value={cookTimeInput}
+        onChangeText={(value) => setCookTimeInput(value)}
+        placeholder="Cook time in minutes"
+        inputMode="numeric"
+        keyboardType="numeric"
+      />
+
+      <DemoDivider size={spacing.lg} />
+
+      <TextField
+        value={servingsInput}
+        onChangeText={(value) => setServingsInput(value)}
+        placeholder="Servings"
+        inputMode="numeric"
+        keyboardType="numeric"
+      />
+
+    </DemoUseCase>
+    </Screen>
   )
 }
 
 const handleSaveRecipe = () => {
+}
+
+const $screenContentContainer: ViewStyle = {
+  flex: 1,
+  marginHorizontal: spacing.md,
+  marginTop: spacing.xl
+}
+
+const $titleContainer: ViewStyle = {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: "center",
+}
+
+const $saveButton: ViewStyle = {
+  minHeight: spacing.md,
 }
