06fd2af077ee1a0dcd86385de738659b28c2923e
Brett Hoes <bretthoes@gmail.com>
2024-10-31 08:47:54 -0400

    add impl for fetch memberships in store

--- app/models/MembershipStore.ts
+++ app/models/MembershipStore.ts
@@ -2,23 +2,25 @@ import { Instance, SnapshotIn, SnapshotOut, types } from "mobx-state-tree"
 import { withSetPropAction } from "./helpers/withSetPropAction"
 import { MembershipListModel } from "./MembershipList"
 import { Membership, MembershipModel } from "./Membership"
+import { api } from "app/services/api"
 
 export const MembershipStoreModel = types
   .model("MembershipStore")
   .props({
-    memberships: types.optional(MembershipListModel, {
-      items: [],
-      pageNumber: 1,
-      totalPages: 1,
-      totalCount: 0,
-    }),
+    memberships: types.maybeNull(MembershipListModel),
     currentMembership: types.maybeNull(types.reference(MembershipModel))
   })
   .actions(withSetPropAction)
   .views((self) => ({}))
   .actions((self) => ({
-    async fetchMemberships() {
-      // TODO implement
+    async fetchMemberships(cookbookId: number, pageNumber = 1, pageSize = 10) {
+      this.clearCurrentMembership()
+      const response = await api.GetMemberships(cookbookId, pageNumber, pageSize)
+      if (response.kind == "ok") {
+        self.setProp("memberships", response.memberships)
+      } else {
+        console.error(`Error fetching cookbooks: ${JSON.stringify(response)}`)
+      }
     },
     setCurrentMembership(membership: Membership) {
       self.currentMembership = membership
