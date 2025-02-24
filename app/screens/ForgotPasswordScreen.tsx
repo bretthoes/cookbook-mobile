c1e706e7074f492383f1a67cc007735b4b12796a
Brett Hoes <bretthoes@gmail.com>
2025-02-24 00:06:34 -0500

    build out forgot password screen

--- app/screens/ForgotPasswordScreen.tsx
+++ app/screens/ForgotPasswordScreen.tsx
@@ -1,26 +1,85 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
 import { AppStackScreenProps } from "app/navigators"
-import { Screen, Text } from "app/components"
-// import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "app/models"
+import { Button, Screen, Text, TextField } from "app/components"
+import { useStores } from "app/models"
+import { useNavigation } from "@react-navigation/native"
+import { spacing } from "app/theme"
 
 interface ForgotPasswordScreenProps extends AppStackScreenProps<"ForgotPassword"> {}
 
 export const ForgotPasswordScreen: FC<ForgotPasswordScreenProps> = observer(function ForgotPasswordScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const {
+      authenticationStore: {
+        authEmail,
+        result,
+        setAuthEmail,
+        forgotPassword,
+        validationError,
+      }
+    } = useStores()
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
+    const navigation = useNavigation<AppStackScreenProps<"ForgotPassword">["navigation"]>()
+  
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    const error = isSubmitted ? validationError : ""
+
+    async function sendForgotPasswordEmail() {
+      setIsSubmitted(true)
+      if (validationError) return
+      setIsSubmitted(false)
+      await forgotPassword()
+      navigation.push("ResetPassword")
+    }
+
   return (
-    <Screen style={$root} preset="scroll">
-      <Text text="forgotPassword" />
-    </Screen>
-  )
-})
+      <Screen
+        style={$root}
+        preset="auto"
+        safeAreaEdges={["top", "bottom"]}
+      >
+        <Text text="Enter your email" preset="heading" />
+        <Text>We'll send a message with instructions to reset your password.</Text>
 
-const $root: ViewStyle = {
-  flex: 1,
-}
+        <TextField
+          value={authEmail}
+          onChangeText={setAuthEmail}
+          containerStyle={$textField}
+          autoCapitalize="none"
+          autoComplete="email"
+          autoCorrect={false}
+          keyboardType="email-address"
+          labelTx="loginScreen.emailFieldLabel"
+          placeholderTx="loginScreen.emailFieldPlaceholder"
+          helper={error}
+          status={error ? "error" : undefined}
+        />
+
+        <Text text={`${result}`} preset="formHelper" />
+  
+        <Button
+          text="Send me the code!"
+          onPress={sendForgotPasswordEmail}
+          style={$tapButton}
+        />
+      </Screen>
+    )
+  })
+  
+  const $root: ViewStyle = {
+    flex: 1,
+    paddingVertical: spacing.xxl,
+    paddingHorizontal: spacing.lg,
+  }
+  
+  
+  const $tapButton: ViewStyle = {
+    marginTop: spacing.xs,
+    marginBottom: spacing.xs
+  }
+  
+  const $textField: ViewStyle = {
+    marginBottom: spacing.lg,
+  }
