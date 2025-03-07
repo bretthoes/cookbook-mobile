c0d9ee91fc0c72ccbbf510ee7e30cf60bd815a53
brett <bretthoes@gmail.com>
2025-03-07 17:04:15 -0500

    add action sheet pkg, wrap app

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import "@expo/metro-runtime"
 import React from "react"
 import * as SplashScreen from "expo-splash-screen"
 import App from "./app/app"
+import { ActionSheetProvider } from "@expo/react-native-action-sheet"
 
 SplashScreen.preventAutoHideAsync()
 
 function IgniteApp() {
-  return <App hideSplashScreen={SplashScreen.hideAsync} />
+  return (
+    <ActionSheetProvider>
+      <App hideSplashScreen={SplashScreen.hideAsync} />
+    </ActionSheetProvider>
+  )
 }
 
 export default IgniteApp
